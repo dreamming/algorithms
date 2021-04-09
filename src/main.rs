cdac8d721cf6ca8892cf3796992d1911ad56c517
Super Mark <supermark@SuperdeMacBook-Pro.local>
2021-04-09 16:07:10 +0800

    some updates

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use rand::Rng;
 use std::time::{Instant};
+use std::fs::File;
 pub mod sorting;
 
 fn main() {
+    let result = File::open("a.txt");
+    // match result {
+    //     Ok(f) => println!(" exist "),
+    //     Err(e) => println!(" exist "),
+    // }
 
-    let mut v = [0;50000];
-    for i in 0..v.len() {
-        let mut rng = rand::thread_rng();
-        let gen_value: u32 = rng.gen();
-        v[i] = gen_value;
-    }
+    // if let Ok(file) = result {
+    //     println!(" exist ");
+    // } else{
+    //     println!(" exist ");
+    // };
 
-    let start_sort = Instant::now();
-    sorting::heap_sort(&mut v);
-    // sorting::quick_sort_recursive(&mut v);
-    let sort_duration = start_sort.elapsed();
-    println!("Time elapsed in sort_duration is: {:?}", sort_duration);
-    assert_eq!(sorting::is_sorted(&v), true);
+    // let x = File::open("a.txt").unwrap();
+    // println!("hello ");
+
+    let x = File::open("a.txt").expect("fail to open file ");
+    // println!("hello {:?}", x);
+    // let mut v = [0;50000];
+    // for i in 0..v.len() {
+    //     let mut rng = rand::thread_rng();
+    //     let gen_value: u32 = rng.gen();
+    //     v[i] = gen_value;
+    // }
+    //
+    // let start_sort = Instant::now();
+    // sorting::heap_sort(&mut v);
+    // // sorting::quick_sort_recursive(&mut v);
+    // let sort_duration = start_sort.elapsed();
+    // println!("Time elapsed in sort_duration is: {:?}", sort_duration);
+    // assert_eq!(sorting::is_sorted(&v), true);
 }
